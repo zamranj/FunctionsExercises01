cd822b6882271530532fab85bd52beef57b51f3c
zeejay21 <84418586+zamranj@users.noreply.github.com>
2021-06-16 01:44:24 +0000

    Bonus 1

--- index.js
+++ index.js
@@ -5,9 +5,22 @@ c) Retrieve only the first 3 characters from strings with lengths larger than 3.
 d) Use a template literal to return the phrase, "We put the '___' in '___'." Fill the first blank with the modified string, and fill the second blank
 with the original string.
 */
+function buildFunPhrase(word){
+  let subWord = "";
+  // word = word.split("");
 
+  if(word.length <= 3){
+    subWord = word.slice(-1);
+  }else{
+    subWord = word.slice(0, 3);
+  }
+  return `We put the ${subWord} in ${word}`;
+}
+
+console.log(buildFunPhrase("fun"));
 
 
 /* Now test your function:
 e) Outside of the function, define the variable str and initialize it with a string (e.g. 'Functions rock!').
 */
+console.log(buildFunPhrase("Functions rock!"));
